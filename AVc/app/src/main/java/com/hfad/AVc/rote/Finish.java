775e5127034044832cd96c90f388150c9f30e3bd
Play-actor <play-actor@mail.ru>
2021-03-02 20:49:27 +0800

    реализован функционал Dagger2

--- AVc/app/src/main/java/com/hfad/AVc/rote/Finish.java
+++
@@ -1,6 +0,0 @@
-package com.hfad.avc.rote;
-
-import ru.terrakok.cicerone.commands.Command;
-
-public class Finish implements Command {
-}
--- AVc/app/src/main/java/com/hfad/AVc/rote/OpenMainActivity.java
+++
@@ -1,8 +0,0 @@
-package com.hfad.avc.rote;
-
-import ru.terrakok.cicerone.commands.Command;
-
-public class OpenMainActivity implements Command {
-    public OpenMainActivity() {
-    }
-}
