f5af7907d7e1ec5d3f04cb1fb003b51df19cc49b
Play-actor <play-actor@mail.ru>
2021-03-20 00:58:54 +0800

    воркер итератор, Intent.ACTION_SEND, работают,  осталось привисти к верной бизнес логике

---
+++ AVc/app/src/main/java/com/hfad/avc/interactor/SendIteractor.java
@@ -0,0 +1,48 @@
+package com.hfad.avc.interactor;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+
+public class SendIteractor {
+    @SuppressLint("IntentReset")
+    public void smsSend(Context context, String toSms, String messageText) {
+        Intent sms;
+        try {
+            sms = new Intent(Intent.ACTION_SEND, Uri.parse("smsto:" + art(toSms)));
+            sms.setType("text/plain");
+            sms.putExtra(Intent.EXTRA_TEXT, messageText);
+            context.startActivity(Intent.createChooser(sms, "Отправить"));
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public String art(String st) {
+        String taboo = "+-";
+        for (char c : taboo.toCharArray()) {
+            st = st.replace(c, ' ');
+            st = st.replaceAll(" ", "");
+        }
+        return st;
+    }
+
+    /*
+     *//**
+     * отправка смс без перехода в смс-менеджер
+     *//*
+    public void smsSend_avto() {
+        try {
+            SmsManager.getDefault()
+                    .sendTextMessage(numberText, null, messageText.toString(), null, null);
+            Toast.makeText(getActivity().getApplicationContext(),
+                    "SMS отправлено!",Toast.LENGTH_LONG).show();
+
+        } catch (Exception ex) {
+            Toast.makeText(getActivity().getApplicationContext(),ex.getMessage().toString(),
+                    Toast.LENGTH_LONG).show();
+            ex.printStackTrace();
+        }
+    }*/
+}
